975c5e0f7953f223717867bc972ad16a1780aa74
Aidan Stranaghan <Aidanstranaghan@gmail.com>
2024-04-14 19:01:11 -0400

    More changes and bug fixes

--- test.js
+++ test.js
@@ -2,7 +2,7 @@
 
 $(document).ready(async function(){ 
     const tableRow = document.querySelectorAll('tr[bgcolor="#DCDCDC"], tr[bgcolor="#C0C0C0"]');
-    const newDiv = document.createElement('div');
+    
 
     const profCache = [];
     // Structure for the rating Popup on hover
@@ -37,7 +37,6 @@ $(document).ready(async function(){
                     aTag.href = `https://www.ratemyprofessors.com/professor/${teacherData.legacyId}`;
                     aTag.target = '_blank';
                     tableRow[i].children[11].replaceWith(aTag);
-
                 }
                 
                 console.log(teacherData);
@@ -46,7 +45,12 @@ $(document).ready(async function(){
                 if (teacherData == undefined || teacherData.avgRating == null || teacherData.legacyId == null || teacherData.avgRating == 0) { //if no rating is found or no profile is found, display --
                     const rate = '--';
                     var circleRate = `<span class="circle" style="background-color: grey;">${rate}</span> `;
-                    tableRow[i].children[10].innerHTML = circleRate;
+                    const newDiv = document.createElement('div');
+                    tableRow[i].children[10].innerHTML = ' '; // Clear existing content
+                    newDiv.textContent = 'R: '; // Use textContent to set text content
+                    newDiv.innerHTML += circleRate; // Use innerHTML to append HTML string
+                    tableRow[i].children[10].appendChild(newDiv); // Append newDiv to tableRow
+                    
                 }
                 else { //if they have a rating, display rating
                     const rate = teacherData.avgRating;
@@ -60,8 +64,15 @@ $(document).ready(async function(){
                     } else if (rate >= 0) { //red
                         var circleRate = `<span class="circle" style="background-color: red;">${newrate}</span>`;
                     }
-                    tableRow[i].children[10].innerHTML = circleRate;
+                    const newDiv = document.createElement('div');
+                    newDiv.className = 'circleContainer';
+                    newDiv.textContent = 'R: '; // Use textContent to set text content
+                    newDiv.innerHTML += circleRate; // Use innerHTML to append HTML string
+                    
+                    tableRow[i].children[10].innerHTML = ' '; // Clear existing content
+                    tableRow[i].children[10].appendChild(newDiv); // Append newDiv to tableRow                    
                 }
+
             }
         }
     }
@@ -70,7 +81,7 @@ $(document).ready(async function(){
 async function getProf(name) {
     try {
         const newName = encodeURIComponent(name);
-        const response = await fetch(`http://localhost:3300/api/getProf/${newName}`, { cache:"force-cache" });
+        const response = await fetch(`http://localhost:3300/api/getProf/${newName}`, { cache:"default" });
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
